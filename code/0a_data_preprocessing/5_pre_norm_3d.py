8d4a52e1e40c5465c5b1340e1e28c82daa30eace
vmos1 <squaredrive@gmail.com>
2021-07-22 10:29:42 -0700

    added fix to pre-norm data extraction code to run norm in sequence to reduce memory footprint. Only saving normed images now

--- code/0a_data_preprocessing/5_pre_norm_3d.py
+++ code/0a_data_preprocessing/5_pre_norm_3d.py
@@ -7,6 +7,7 @@
 import numpy as np
 import time
 import sys
+import os
 
 def f_scaling_transform(model,samples):
     ''' Read in the transformation function and samples array
@@ -43,49 +44,83 @@ def f_scaling_transform(model,samples):
         
         return np.vectorize(f_transform)(samples)
     
+    
+def f_concat_temp_files(num_batches,save_location,file_prefix):
+    '''
+    Function to concatenate temp files to create the full file.
+    Steps: get data from temp files, stack numpy arrays and delete temp files
+    '''
+    if num_batches<1:
+        print('zero temp files',num_batches)
+        return 0
+    
+    x = np.vstack([np.load(save_location+'temp_data_%s_%s'%(file_prefix,count)+'.npy') for count in np.arange(num_batches)])
+    
+    # Delete temp files
+    for count in np.arange(num_batches):
+        prefix='temp_data_%s_%s'%(file_prefix,count)
+        f1=prefix+'.npy'
+        os.remove(save_location+f1)
+    print("Deleted temp files")
+    
+    return x
+
+
 if __name__=='__main__':
-    
-    train_size,val_size=np.int(65000),500
-#     train_size,val_size=np.int(18e3),3000
+
     model=1 # Transformation model
-    
     t1=time.time()
-#     data_dir='/global/cfs/cdirs/m3363/vayyar/cosmogan_data/raw_data/3d_data/dataset5_3dcgan_4univs_64cube_simple_splicing/'
-#     ip_fname=data_dir+'Om0.3_Sg0.8_H70.0.npy'
-
-    data_dir='/global/cfs/cdirs/m3363/vayyar/cosmogan_data/raw_data/temp_data/'
+    
     ip_fname=sys.argv[1]
-
     print("file",ip_fname)
-
+    
+    output_dir='/global/cfs/cdirs/m3363/vayyar/cosmogan_data/raw_data/temp_data/'
+    file_prefix='norm'
+    
     ### Read data
-    samples = np.load(ip_fname,allow_pickle=True)
-    print(samples.shape)
+    samples = np.load(ip_fname,mmap_mode='r',allow_pickle=True).copy()
+    data_size=samples.shape[0]
+    print(data_size)
+    train_size,val_size=int(0.9*data_size),int(0.1*data_size)
     
-    ### Random slice to extract a smaller, required sub-sample
-    # Since the lbann code has typical validation ratio of 0.8, we need to grab more samples so that keras and lbann samples match
-    select_size=np.int((train_size+val_size)/0.8)+1
-    print(select_size)
-    
+    num_per_batch=100
+    num_batches=int(data_size/num_per_batch)+1
+    print("Num batches",num_batches)
     np.random.seed=27705
-#     samples=np.random.choice(samples,size=select_size,replace=False)
-    t2=time.time()
-    np.random.shuffle(samples)
-    t3=time.time()
-    print("Time for shuffle",t3-t2)
-    samples=samples[:select_size]
-#     np.save(data_dir+'train.npy',samples[:train_size]) ## Not saving training data
-    np.save(data_dir+'val.npy',samples[train_size:(train_size+val_size)])
     
-    ### Transform the images 
+    for count in range(num_batches):
+#         print(count*num_per_batch,(count+1)*num_per_batch)
+        arr=samples[count*num_per_batch:(count+1)*num_per_batch]
+        ## Shuffle data
+        t2=time.time()
+        np.random.shuffle(arr)
+        t3=time.time()
+        print("Time for shuffle",t3-t2)
+    #     np.save(data_dir+'train.npy',samples[:train_size]) ## Not saving training data
+    #     np.save(data_dir+'val.npy',samples[train_size:(train_size+val_size)])
+
+        ### Transform the images 
+        t4=time.time()
+        arr2=f_scaling_transform(model,arr)
+        t5=time.time()
+        print("Time for Applying transform",t5-t4)
+        print(arr2.shape,arr.shape)
+        prefix='temp_data_{0}_{1}'.format(file_prefix,count)
+        np.save(output_dir+prefix+'.npy',arr2)
+        t4=time.time()
+        print("Extraction time for count ",count,":",t4-t3)
+    
+    del samples
+    ### Concatenate temp files
     t4=time.time()
-    samples=f_scaling_transform(model,samples)
+    img=f_concat_temp_files(num_batches,save_location=output_dir,file_prefix=file_prefix)
     t5=time.time()
-    print("Time for Applying transform",t5-t4)
-    print(samples.shape,type(samples[0,0,0,0,0]))
+    print("Time for concatenation of file:",t5-t4)
+    print("Total number of images",img.shape)
     
     ### Save to output files
-    np.save(data_dir+'norm_{0}_train_val.npy'.format(model),samples)
+    np.save(output_dir+'norm_{0}_train_val.npy'.format(model),img)
     
     t6=time.time()
     print("Time for saving file",t6-t5)
+    print("Total time",t6-t1)
